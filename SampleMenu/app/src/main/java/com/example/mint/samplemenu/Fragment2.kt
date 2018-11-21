1f3d04680b2f08e77f6dfe7107049d9068f4662a
Fahim Ahmed Shakil <fahimahmed38@gmail.com>
2018-11-21 23:39:43 +0600

    new2 activity added

--- SampleMenu/app/src/main/java/com/example/mint/samplemenu/Fragment2.kt
+++ SampleMenu/app/src/main/java/com/example/mint/samplemenu/Fragment2.kt
@@ -1,12 +1,13 @@
 package com.example.mint.samplemenu
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import kotlinx.android.synthetic.main.fragment_fragment2.view.*
 
 
 class Fragment2 : Fragment() {
@@ -14,7 +15,16 @@ class Fragment2 : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment2, container, false)
+        val view = inflater.inflate(R.layout.fragment_fragment2, container, false)
+
+        view.apply {
+            button2.setOnClickListener{
+                val intent = Intent(this@Fragment2.context,NewActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        return view
     }
 
 

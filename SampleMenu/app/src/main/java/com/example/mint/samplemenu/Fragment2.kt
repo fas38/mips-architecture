bba9e6cffcf0eb8a589f2d44b98b98816a772a79
Fahim Ahmed Shakil <fahimahmed38@gmail.com>
2019-01-09 01:09:41 +0600

    Revert "new2 activity added"
    This reverts commit 1f3d04680b2f08e77f6dfe7107049d9068f4662a.

--- SampleMenu/app/src/main/java/com/example/mint/samplemenu/Fragment2.kt
+++ SampleMenu/app/src/main/java/com/example/mint/samplemenu/Fragment2.kt
@@ -1,13 +1,12 @@
 package com.example.mint.samplemenu
 
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_fragment2.view.*
+
 
 
 class Fragment2 : Fragment() {
@@ -15,16 +14,7 @@ class Fragment2 : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_fragment2, container, false)
-
-        view.apply {
-            button2.setOnClickListener{
-                val intent = Intent(this@Fragment2.context,NewActivity::class.java)
-                startActivity(intent)
-            }
-        }
-
-        return view
+        return inflater.inflate(R.layout.fragment_fragment2, container, false)
     }
 
 
